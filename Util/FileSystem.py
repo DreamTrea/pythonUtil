a940bb6629f994d355da4e94cd5f50050ff7d639
in_hwan <cjhih43@gmail.com>
2018-12-19 22:19:16 +0900

    S3 버킷 사용 수정

--- Util/FileSystem.py
+++ Util/FileSystem.py
@@ -1,7 +1,8 @@
 import os
 #https://github.com/RaRe-Technologies/smart_open
 from smart_open import smart_open
-from Util.JsonUtil import JsonUtil as ju
+from src.Util.JsonUtil import JsonUtil as ju
+import boto3
 
 class FileSystem(object):
 
@@ -15,7 +16,7 @@ class FileSystem(object):
         file_path_list = []
         for file_path, dir, files in os.walk(base_dir):
             for filename in files:
-                full_filename = '\\'.join([file_path, filename])
+                full_filename = '/'.join([file_path, filename])
                 ext = os.path.splitext(filename)[-1]
                 if ext == sign:
                     file_path_list.append(full_filename)
@@ -51,13 +52,34 @@ class FileSystem(object):
     data : directory data or file data path
     """
     @staticmethod
-    def uploadToS3(type,server_path,file_path,data):
-        with smart_open("//".join(server_path,file_path),'wb',encoding='utf8') as fout:
+    def uploadToS3(type,file_name,data,localPath=''):
+        system_path = os.getcwd()+'/'
+        config_data = ju.jsonToDictionary(FileSystem.getDataFromFile(system_path+"config.json"))
 
-            if type.equal("dictionary"):
-                fout.write(ju.dictionaryToJson(data))
-
-            elif type.equal("file"):
-                for line in smart_open(data,encoding='utf8'):
-                    fout.write(line)
-        pass
+        config_data = config_data.get('s3_bucket')
+        if type=='file' :
+            client = boto3.client('s3',
+                    region_name=config_data.get('region_name'),
+                    aws_access_key_id=config_data.get('accesskey'),
+                    aws_secret_access_key=config_data.get('secretkey')
+                )
+            client.upload_file(file_name,config_data.get('serverPath'),data)
+        else:
+            session = boto3.resource('s3',
+                region_name=config_data.get('region_name'),
+                aws_access_key_id=config_data.get('accesskey'),
+                aws_secret_access_key=config_data.get('secretkey')
+            )
+            session.Bucket(config_data.get('serverPath')).put_object(Key=file_name,Body=data)
+#
+#    @staticmethod
+#    def uploadToS3(type,server_path,file_name,data):
+#        with smart_open("//".join(server_path,file_path),'wb',encoding='utf8') as fout:
+#
+#            if type.equal("dictionary"):
+#                fout.write(ju.dictionaryToJson(data))
+#
+#            elif type.equal("file"):
+#                for line in smart_open(data,encoding='utf8'):
+#                    fout.write(line)
+#        pass
